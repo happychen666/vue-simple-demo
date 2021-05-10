be7088a136243e399ec6cae30b9cfcda283c4ccd
Happy Chen <2695401775@qq.com>
2021-05-10 21:47:20 +0800

    vue的生命周期

---
+++ src/app-prop-function.vue
@@ -0,0 +1,28 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <template>
+      <ChildComponent :onClick="sampleFunction" :fruits="sampleData" />
+    </template>
+  </div>
+</template>
+ 
+<script>
+import ChildComponent from "./props-function/Child.vue";
+export default {
+  name: "App",
+  components: {
+    ChildComponent,
+  },
+  data() {
+    return {
+      sampleData: ["Apple", "Banana", "Mango", "Strawberry"],
+    };
+  },
+  methods: {
+    sampleFunction(fruitName) {
+      alert("You have selected: " + fruitName);
+    },
+  },
+};
+</script>
--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,7 @@
 <!-- App.vue -->
 <template>
   <div id="app">
+    <p>{{ text }}</p>
     <template>
       <ChildComponent :onClick="sampleFunction" :fruits="sampleData" />
     </template>
@@ -16,6 +17,7 @@ export default {
   },
   data() {
     return {
+      text: 0,
       sampleData: ["Apple", "Banana", "Mango", "Strawberry"],
     };
   },
@@ -23,6 +25,40 @@ export default {
     sampleFunction(fruitName) {
       alert("You have selected: " + fruitName);
     },
+  },
+  beforeCreate() {
+    console.log(this.$el, "beforeCreate");
+  },
+  created() {
+    console.log(this.$el, "created");
+  },
+  beforeMount() {
+    console.log(this.$el, "beforeMount");
+  },
+  mounted() {
+    console.log(this.$el, "mounted");
+    // 测试 beforeUpdate，updated会在数据更新时执行
+    // setInterval(() => {
+    //   this.text++;
+    // }, 1000);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  activated() {
+    console.log("activated");
+  },
+  deactivated() {
+    console.log("deactivated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
   },
 };
 </script>
